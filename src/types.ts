e24b6ed4b6cba46cf4fa25bd3f4d15b696d43e58
Jeffrey Chupp <jeff@semanticart.com>
2023-11-14 16:23:52 -0500

    add valueOf

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,19 @@
+import type { Prefab } from "@prefab-cloud/prefab-cloud-node";
+
 export type Logger = (category: any, message: unknown) => void;
+
+export type PrefabConfig = Exclude<
+  ReturnType<typeof Prefab.prototype.raw>,
+  undefined
+>;
+
+export type GetValue = Exclude<
+  ReturnType<typeof Prefab.prototype.get>,
+  undefined
+>;
+
+export type ConfigRow = PrefabConfig["rows"][0];
+
+export type ConditionalValue = ConfigRow["values"][0];
+
+export type ConfigValue = Exclude<ConditionalValue["value"], undefined>;
---
+++ src/valueOf.ts
@@ -0,0 +1,51 @@
+import { type ConfigValue, type GetValue } from "./types.js";
+
+// TODO: remove this when the proto includes `provided`
+export type Provided = {
+  provided: {
+    lookup: string;
+  };
+};
+
+export const valueOf = (value: ConfigValue): GetValue => {
+  switch (Object.keys(value)[0]) {
+    case "string":
+      return value.string;
+    case "stringList":
+      return value.stringList?.values;
+    case "int":
+      return Number(value.int);
+    case "bool":
+      return value.bool;
+    case "double":
+      return Number(value.double);
+    case "logLevel":
+      return value.logLevel;
+    case "weightedValues":
+      return value.weightedValues?.weightedValues
+        .sort((a, b) => b.weight - a.weight)
+        .map((weightedValue) => {
+          const value = weightedValue.value ? valueOf(weightedValue.value) : "";
+          return `${value}: ${weightedValue.weight}%`;
+        })
+        .join(", ");
+    case "provided":
+      return (value as Provided).provided.lookup;
+    default:
+      throw new Error(`Unexpected value ${JSON.stringify(value)}`);
+  }
+};
+
+export const valueOfToString = (value: ConfigValue): string => {
+  const v = valueOf(value);
+
+  if (typeof v === "string") {
+    return v;
+  }
+
+  if (Array.isArray(v)) {
+    return v.join(", ");
+  }
+
+  return JSON.stringify(v);
+};
