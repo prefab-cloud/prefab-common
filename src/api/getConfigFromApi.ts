a801a98ca5c415f5ddad83c0cc97fca4a7e91313
Jeffrey Chupp <jeff@semanticart.com>
2023-11-20 12:20:35 -0500

    Add getConfigFromApi

---
+++ src/api/getConfigFromApi.ts
@@ -0,0 +1,32 @@
+import { Client } from "./client.js";
+import type { Logger, PrefabConfig } from "../types.js";
+
+export const getConfigFromApi = async ({
+  client,
+  errorLog,
+  key,
+}: {
+  client: Client;
+  errorLog: Logger;
+  key: string;
+}): Promise<PrefabConfig | undefined> => {
+  const requestPath = `/api/v1/config/key/${encodeURIComponent(key)}`;
+
+  const response = await client.get(requestPath);
+
+  if (response.status !== 200) {
+    const text = response.text();
+    const statusText = response.statusText;
+
+    errorLog("PrefabClient", {
+      message: "Error fetching config",
+      response,
+      text,
+      statusText,
+    });
+
+    return;
+  }
+
+  return await response.json();
+};
